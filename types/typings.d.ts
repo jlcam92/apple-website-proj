25645c8d724e02ce8f787ffdedc9905a55de6d9a
jlcam92 <camunasluis@gmail.com>
2024-10-15 17:01:25 -0300

    Init apple website project w/typescript

---
+++ types/typings.d.ts
@@ -0,0 +1,11 @@
+declare module "utils" {
+	export const blackImg;
+	export const blueImg;
+	export const highlightFirstVideo;
+	export const highlightFourthVideo;
+	export const highlightSecondVideo;
+	export const highlightThirdVideo;
+	export const whiteImg;
+	export const yellowImg;
+	export const appleImg;
+}
